9fcb655230debe123be77a12ce47c3d854426781
tolerevo <tolerevo@hotmail.com>
2024-01-18 00:19:42 -0600

    finish contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,3 +1,4 @@
+import { toBeRequired } from "@testing-library/jest-dom/matchers";
 import React from "react";
 
 export const ContactForm = ({
@@ -7,10 +8,48 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Contact Name"
+          aria-label="Contact Name"
+          required
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="tel"
+          value={phone}
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          placeholder="Contact Phone (###-###-####)"
+          pettern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          aria-label="Contact Phone"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email"
+          aria-label="Contact Email"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" aria-label="Add Contact" />
+    </form>
   );
 };
-
