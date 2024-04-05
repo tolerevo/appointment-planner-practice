6b4a19cb7ec4f533c2cab52b5c53f8c6cd9d31f5
tolerevo <tolerevo@hotmail.com>
2024-04-04 19:54:38 -0600

    add tiles array to TilesList component

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({ tiles }) => {
   return (
     <div>
-      
+      {tiles.map((tile, index) => {
+        const { name, ...description } = tile;
+        return <Tile tile={name} key={index} description={description} />;
+      })}
+      ;
     </div>
   );
 };
