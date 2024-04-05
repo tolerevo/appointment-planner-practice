88703797cd627aeaca6b2cbfce15636855859e64
tolerevo <tolerevo@hotmail.com>
2024-04-04 18:02:36 -0600

    add contact form with props and handlesubmit

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,10 +7,48 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Contact Name"
+          aria-label="Contact Name"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="tel"
+          value={phone}
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          placeholder="Contact Phone (##-##-####)"
+          aria-label="Contact Phone"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="email"
+          value={email}
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Contact Email"
+          aria-label="Contact Email"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" aria-label="Add Contact" />
+    </form>
   );
 };
-
